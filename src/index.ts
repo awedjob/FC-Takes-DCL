5b81decf7484352e754970e44a5f6fca9fb64ef3
Scott Moore <scott@affirmative.org>
2026-06-15 15:45:36 -0500

    Remove back plane - rely on alpha transparency for below-view invisibility

--- src/index.ts
+++ src/index.ts
@@ -123,16 +123,4 @@ function createSkyLogo() {
     }),
     transparencyMode: MaterialTransparencyMode.MTM_ALPHA_BLEND
   })
-
-  // Create a back-blocking plane (opaque black, faces opposite direction to block view from below)
-  const backEntity = engine.addEntity()
-  MeshRenderer.setPlane(backEntity)
-  Transform.create(backEntity, {
-    position: { x: 0, y: 76.1, z: 32 },
-    scale: { x: 32, y: 32, z: 1 },
-    rotation: Quaternion.fromEulerDegrees(-90, 0, 0)
-  })
-  Material.setPbrMaterial(backEntity, {
-    albedoColor: Color4.Black()
-  })
 }
